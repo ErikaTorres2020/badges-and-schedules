fa7117d44b7688aa4e29c34efc77f2fd2387a0c3
Erika Torres <+>
2021-03-01 20:14:37 -0500

    commit tree ec934f87a9174ad2cea90390f4bcb388af302c21

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+   "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |print|
+    "Hello, my name is #{print}."
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.map.with_index(1) do |speaker,room|
+    "Hello, #{speaker}! You'll be assigned to room #{room}!"
+  end
+  
+end
+
+def printer(speakers)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(speakers).each do |room|
+    puts room 
+  end
+end
